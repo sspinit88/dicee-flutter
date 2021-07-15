592f7d7aff8557c169e4c60d300924ca87013eff
Sergey Pashnin <sspinit88@gmail.com>
2021-07-15 11:41:37 +0300

    add Expanded()

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          title: Text(
+            'Dicee',
+          ),
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -18,6 +20,19 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      // TODO Виджет Row располагает элементы горизонтально, в виде строки.
+      // https://metanit.com/dart/flutter/2.8.php
+      children: <Widget>[
+        Expanded(
+          // TODO Контейнер Expanded позволяет своему вложенному виджету child заполнить доступное пространство (или его часть) других контейнеров - Row и Column.
+          // https://metanit.com/dart/flutter/2.9.php
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          child: Image.asset('images/dice2.png'),
+        ),
+      ],
+    );
   }
 }
