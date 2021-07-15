7b0916831083e36bcd7244349883530468d2800c
Sergey Pashnin <sspinit88@gmail.com>
2021-07-15 13:58:30 +0300

    done!

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,22 +18,55 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 2;
+  int maxDiceNumber = 6;
+
+  int getRandomDiceNumber() => Random().nextInt(maxDiceNumber) + 1;
+  void changeDice() {
+    leftDiceNumber = getRandomDiceNumber();
+    rightDiceNumber = getRandomDiceNumber();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      // TODO Виджет Row располагает элементы горизонтально, в виде строки.
-      // https://metanit.com/dart/flutter/2.8.php
-      children: <Widget>[
-        Expanded(
-          // TODO Контейнер Expanded позволяет своему вложенному виджету child заполнить доступное пространство (или его часть) других контейнеров - Row и Column.
-          // https://metanit.com/dart/flutter/2.9.php
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        // TODO Виджет Row располагает элементы горизонтально, в виде строки.
+        // https://metanit.com/dart/flutter/2.8.php
+        children: <Widget>[
+          Expanded(
+            // TODO Контейнер Expanded позволяет своему вложенному виджету child
+            //  заполнить доступное пространство (или его часть) других
+            //  контейнеров - Row и Column.
+            // https://metanit.com/dart/flutter/2.9.php
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  changeDice();
+                });
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  changeDice();
+                });
+              },
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
